0084554d60d369572371025233bab2e090d84170
Chukwuemelie Oguchi <chukwuemelie4@gmail.com>
2018-01-10 20:00:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 // write your code below!
 // 1 begins
-//1. Write a function named `happyHolidays`. This function should not accept any parameters and should return the string `""`.
 function happyHolidays(){
   return `Happy holidays!`}
 console.log (happyHolidays())
+/*
+3. Write a function named `happyHolidayTo`. This function should accept two parameters, the holiday you want to wish them well for,
+ and the name of the person you're wishing well. This function should return the string `` `Happy ${holiday}, ${name}!` ``
+4. Write a function named `holidayCountdown`. This function should accept two parameters, 
+the holiday name and number of days till that holiday. The function should return the string `` `It's ${days} days until ${holiday}!` ``
+*/
+// 2 begins
+function happyHolidaysTo(name){
+return `Happy holidays, ${name}!`  
+}
+console.log(happyHolidaysTo(Adrienne))
+// 3 begins
+function happyHolidayTo(holiday,name){
+  return `Happy ${holiday}, ${name}!`
+}
+console.log(happyHolidayTo(Valentine day, Jennifer))
+// 4 begins
+function holidayCountdown(holiday,days){
+  return `It's ${days} days until ${holiday}!`
+}
+console.log(holidayCountdown(your birthday,4))
