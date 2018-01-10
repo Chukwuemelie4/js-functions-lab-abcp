bcbd1bc92793d3a6764e02faa53adc8d97a6a023
Chukwuemelie Oguchi <chukwuemelie4@gmail.com>
2018-01-10 19:44:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // write your code below!
 // 1 begins
 //1. Write a function named `happyHolidays`. This function should not accept any parameters and should return the string `""`.
-function happyHolidays(para){
-if (para==false){
+function happyHolidays(){
   return `Happy holidays!`}
- }
 console.log (happyHolidays())
